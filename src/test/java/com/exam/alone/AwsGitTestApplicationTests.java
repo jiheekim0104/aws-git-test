f440e8385e02345568ab8bef86f60544a6c0c1d6
jiheekim0104 <bearitlove@naver.com>
2023-05-30 16:05:02 +0900

    보냅니다~~~

---
+++ src/test/java/com/exam/alone/AwsGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.alone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
